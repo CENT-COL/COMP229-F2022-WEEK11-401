afac8c9c5e3d703e7cb4aed8430018c4fc517f2c
Thiago Castilho <tdecasti@my.centennialcollege.ca>
2022-11-21 22:14:12 -0500

    week 11 final code (error on add form to be fixed)

--- src/app/content/movies/add/add.component.ts
+++ src/app/content/movies/add/add.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { MoviesService } from 'src/app/services/movies.service';
 
 @Component({
   selector: 'app-add',
@@ -7,9 +9,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MovieAddComponent implements OnInit {
 
-  constructor() { }
+  form: any = {
+    name: String, 
+    year: String, 
+    director: String,
+    genre: String,
+    runtime: Number
+  }
+
+  isSuccessfull = true;
+  errorMessage = "";
+
+  constructor(private movieService: MoviesService, private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit():void {
+    this.movieService.addMovie(this.form)
+      .subscribe({
+        next: data => {
+          console.log(data);
+          this.isSuccessfull = true;
+          this.backToList();          
+        }, 
+        error: err => {
+          this.errorMessage = err.error.message;
+          this.isSuccessfull = false;
+        }
+      });
+  }
+  
+  backToList(): void {
+    this.router.navigate(['/books/list']);
   }
 
 }
